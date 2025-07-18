e8e44aeaae047a32c3ebd74054484b4b9c79d665
sineom <sineom@126.com>
2025-07-18 12:05:24 +0800

    优化

---
+++ build.sh
@@ -0,0 +1,8 @@
+docker build --platform linux/amd64 -t new-api:x86_64 .###
+ # @Author: sineom sineom@126.com
+ # @Date: 2025-07-18 12:04:53
+ # @LastEditors: sineom sineom@126.com
+ # @LastEditTime: 2025-07-18 12:04:55
+ # @FilePath: /new-api/build.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
